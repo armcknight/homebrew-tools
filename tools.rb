e7b0fabb0ca9a6ad8ad0e64c6d41656be0aa4339
Andrew McKnight <git@andrew.mcknight.io>
2026-04-16 16:04:33 -0800

    update to 4.0.0

--- tools.rb
+++ tools.rb
@@ -1,7 +1,7 @@
 class Tools < Formula
   desc "Collection of developer tools for versioning, changelogs, Xcode builds, and more"
   homepage "https://github.com/armcknight/tools"
-  url "https://github.com/armcknight/tools.git", tag: "3.0.0"
+  url "https://github.com/armcknight/tools.git", tag: "4.0.0"
   license "MIT"
 
   depends_on xcode: ["16.0", :build]
